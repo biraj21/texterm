5a3fe812e33a1df55bb138a9a77d44f138dbd1d2
Noah <1101qbtext@gmail.com>
2021-06-05 13:53:40 -0400

    Added more preprocessor tokens and keywords from C99 and C11.

--- includes/languages/c.h
+++ includes/languages/c.h
@@ -8,7 +8,21 @@ char *c_exts[] = {".c", ".h", ".cpp", NULL};
 char *c_keywords[] = {
     "auto", "break", "case", "continue", "do", "default", "else",
     "extern", "for", "if", "goto", "register", "return", "sizeof",
-    "switch", "volatile", "while",
+    "switch", "volatile", "while", "restrict",
+
+    /* C11 */
+    "_Alignas",
+    "_Alignof",
+    "_Atomic",
+    "_Generic",
+    "_Noreturn",
+    "_Static_assert",
+    "_Thread_local",
+
+    /* C99 */
+    "_Bool",
+    "_Complex",
+    "_Imaginary,
 
     // types, variables & functions related
     "char|", "const|", "double|", "enum|", "float| ", "int|", "long|",
@@ -16,7 +30,7 @@ char *c_keywords[] = {
     "void|", NULL};
 
 char *c_preprocs[] = {
-    "define", "elif", "else", "endif", "error", "if", "ifdef",
+    "define", "defined", "elif", "else", "endif", "error", "if", "ifdef",
     "ifndef", "include", "undef", "pragma", NULL};
 
 #endif
