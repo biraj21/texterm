6cd362fa195abfbe439c2540f19707e473227652
Noah <1101qbtext@gmail.com>
2021-06-05 15:24:43 -0400

    Added keywords for C++11, C++17 and C++20

--- includes/languages/c.h
+++ includes/languages/c.h
@@ -13,16 +13,81 @@ char *c_keywords[] = {
     /* C11 */
     "_Alignas",
     "_Alignof",
-    "_Atomic",
+    "_Atomic|",
     "_Generic",
     "_Noreturn",
     "_Static_assert",
-    "_Thread_local",
+    "_Thread_local|",
 
     /* C99 */
-    "_Bool",
-    "_Complex",
-    "_Imaginary,
+    "_Bool|",
+    "_Complex|",
+    "_Imaginary|",
+
+    /* C++ */
+    "and",
+    "and_eq",
+    "asm",
+    "bitand",
+    "bitor",
+    "catch",
+    "class",
+    "compl",
+    "const_cast",
+    "delete",
+    "dynamic_cast",
+    "explicit",
+    "export",
+    "friend",
+    "mutable",
+    "namespace",
+    "new",
+    "not",
+    "not_eq",
+    "operator",
+    "or",
+    "or_eq",
+    "private",
+    "protected",
+    "public",
+    "reinterpret_cast",
+    "static_cast",
+    "template",
+    "this",
+    "throw",
+    "try",
+    "typeid",
+    "typename",
+    "virtual",
+    "wchar_t|",
+    "xor",
+    "xor_eq",
+
+    /* C++11 */
+    "alignas",
+    "alignof",
+    "constexpr",
+    "decltype",
+    "final",
+    "noexcept",
+    "nullptr",
+    "override",
+    "static_assert",
+    "thread_local",
+
+    /* C++20 */
+    "char8_t|",
+    "char16_t|",
+    "char32_t|",
+    "concept",
+    "consteval",
+    "constinit",
+    "co_await",
+    "co_return",
+    "co_yield",
+    "import",
+    "module",
+    "requires",
 
     // types, variables & functions related
     "char|", "const|", "double|", "enum|", "float| ", "int|", "long|",
@@ -31,6 +96,14 @@ char *c_keywords[] = {
 
 char *c_preprocs[] = {
     "define", "defined", "elif", "else", "endif", "error", "if", "ifdef",
-    "ifndef", "include", "undef", "pragma", NULL};
+    "ifndef", "include", "undef", "pragma",
+
+    /* C++17 */
+    "__has_include",
+
+    /* C++20 */
+    "__has_cpp_attribute",
+
+    NULL};
 
 #endif
